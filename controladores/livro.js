9a289014f74b871e282d334265df302a98c85215
Bruno Nogueira Burian <151679524+BruBurian@users.noreply.github.com>
2024-05-03 15:46:49 -0300

    Add files via upload

--- controladores/livro.js
+++ controladores/livro.js
@@ -1,8 +1,9 @@
-const { getTodosLivros } = require("../servicos/livro")
+const { getTodosLivros, getLivroPorId, insereLivro, modificaLivro, deletaLivroPorId } = require("../servicos/livro")
+
 
 function getLivros(req, res) {
     try{
-        const livros = getTodosLivros
+        const livros = getTodosLivros()
         res.send(livros)
     } catch (error) {
         res.status(500)
@@ -10,6 +11,80 @@ function getLivros(req, res) {
     }
 }
 
+function getLivro(req, res) {
+    try{
+        const id = req.params.id
+
+        if(id && Number(id)) {
+            const livro = getLivroPorId(id)
+            res.send(livro)
+            // Nummber(2) -> true
+            // Number("Bruno") -> Nan(not a number)
+        } else{
+            res.status(422)
+            res.send("Id Inválido")
+        }
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function postLivro(req, res) {
+    try {
+        const livroNovo = req.body
+        if(req.body.nome) {
+            insereLivro(livroNovo)
+            res.status(201)
+            res.send("Livro inserido com sucesso")
+        } else{
+            res.status(402)
+            res.send("O campo nome é obrigatório")
+        }
+    } catch(error){
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function patchLivro(req, res) {
+    try{
+        const id = req.params.id
+
+        if(id && Number(id)){
+            const body = req.body
+            modificaLivro(body, id)
+            res.send("Item modificado com sucesso!")
+        } else{
+            res.status(422)
+            res.send("Id Inválido")
+        }
+    }catch(error){
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function deleteLivro(req, res){
+    try{
+        const id = req.params.id
+
+        if(id && Number(id)){
+            deletaLivroPorId(id)
+            res.send("livro deletado com sucesso")
+        }else{
+            res.status(422)
+            res.send("ID inválido")
+        }
+    }catch(error){
+        res.status(500)
+        res.send(error.message)
+    }
+}
 module.exports = {
-    getLivros
+    getLivros,
+    getLivro,
+    postLivro,
+    patchLivro,
+    deleteLivro
 }
